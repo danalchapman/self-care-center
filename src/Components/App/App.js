b15ac8d7dc078c8ecc5d288258870421536bdb15
Dana Chapman <dana.leigh.dlc@gmail.com>
2023-07-06 17:59:36 -0500

    Add Center Component structure and adjust App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Header } from '../Header/Header'
+import { Center } from '../Center/Center'
 import { Footer } from '../Footer/Footer'
 import './App.css'
 
 export const App = () => {
   return (
-    <main>
+    <main className='main-container'>
       <Header />
-      {/* Center */}
+      <Center />
       <Footer />
     </main>
   );
